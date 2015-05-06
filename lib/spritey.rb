a89ea697a75fc84e0a25d370f8a31c122aa84a75
Juan F. Giménez Silva <juanfgs@gmail.com>
2015-05-06 14:51:50 -0300

    fixed version number and some parameters issues

--- lib/spritey.rb
+++ lib/spritey.rb
@@ -1,80 +1,81 @@
 require 'rmagick'
 require 'pathname'
 
-class Spritey
-  attr_accessor :images,:max_height
-  
-  def initialize
-    @images = []
-    @max_height = 0
-  end
-
-  def add_image(filename)
-    image = load_image(filename)
-    @max_height = image.rows unless image.rows < @max_height
-    @images.push image
-  end
-
-
-
-  def save_css(filename)
-    concatenate(filename + ".png")
-    css_file = File.open(filename +".css", "w")
-    css_file.write generate_sprites_css
-    css_file.close
-  end
-  
-  def load_image(filename)
-    Magick::Image::read(filename)[0]    
-  end
-
-  def concatenate(dest_image_filename)
-    raise RuntimeError unless @images.any?
-
-    new_image = Magick::Image.new(total_width,@max_height){
-      self.format = 'png'
-      self.background_color = 'none'
-    }
+module Spritey
+  class Spritey
+    attr_accessor :images,:max_height
     
-    last_width = 0
+    def initialize
+      @images = []
+      @max_height = 0
+    end
 
-    @images.each do |img|
-      img.alpha Magick::BackgroundAlphaChannel
-      new_image.composite!(img,last_width, 0, Magick::OverCompositeOp )
-      last_width += img.columns      
+    def add_image(filename)
+      image = load_image(filename)
+      @max_height = image.rows unless image.rows < @max_height
+      @images.push image
     end
-    puts "Total image size #{total_width},#{@max_height}"    
-    new_image.write(dest_image_filename)
+
+
+
+    def save_css(filename)
+      concatenate(filename + ".png")
+      css_file = File.open(filename +".css", "w")
+      css_file.write generate_sprites_css(filename)
+      css_file.close
+    end
+    
+    def load_image(filename)
+      Magick::Image::read(filename)[0]    
+    end
+
+    def concatenate(dest_image_filename)
+      raise RuntimeError unless @images.any?
+
+      new_image = Magick::Image.new(total_width,@max_height){
+        self.format = 'png'
+        self.background_color = 'none'
+      }
+      
+      last_width = 0
+
+      @images.each do |img|
+        img.alpha Magick::BackgroundAlphaChannel
+        new_image.composite!(img,last_width, 0, Magick::OverCompositeOp )
+        last_width += img.columns      
+      end
+      puts "Total image size #{total_width},#{@max_height}"    
+      new_image.write(dest_image_filename)
+    end
+
+    def generate_sprites_css(filename)
+      last_width = 0
+      dest_css = ""
+      @images.each do |img|
+        image_name = Pathname.new(img.filename).basename.to_s.split(".")
+        dest_css += "##{image_name[0]} {"
+        dest_css += "display:block;"
+        dest_css += "width:#{img.columns}px;"
+        dest_css += "height:#{img.rows}px;"      
+        dest_css += "background-image:url(#{filename}.png);"
+        dest_css += "background-repeat:no-repeat;"      
+        dest_css += "background-position: -#{last_width}px 0px ;"      
+        dest_css += "}\n"
+        last_width += img.columns
+      end
+      dest_css
+    end
+    
+    def total_width
+      width = 0
+      @images.each do |image|
+        width += image.columns
+      end
+      width
+    end
+    
   end
 
-  def generate_sprites_css
-    last_width = 0
-    dest_css = ""
-    @images.each do |img|
-      image_name = Pathname.new(img.filename).basename.to_s.split(".")
-      dest_css += "##{image_name[0]} {"
-      dest_css += "display:block;"
-      dest_css += "width:#{img.columns}px;"
-      dest_css += "height:#{img.rows}px;"      
-      dest_css += "background-image:url(composite.png);"
-      dest_css += "background-repeat:no-repeat;"      
-      dest_css += "background-position: -#{last_width}px 0px ;"      
-      dest_css += "}\n"
-      last_width += img.columns
-    end
-    dest_css
-  end
-  
-  def total_width
-    width = 0
-    @images.each do |image|
-      width += image.columns
-    end
-    width
-  end
-  
 end
-
-
 
 
--- spritey.gemspec
+++ spritey.gemspec
@@ -1,8 +1,8 @@
 # coding: utf-8
 Gem::Specification.new do |s|
-  s.name = 'Spritey'
+  s.name = 'spritey'
   s.version = '0.0.1'
-  s.date = '2015-02-08'
+  s.date = '2015-05-06'
   s.summary = "A simple sprite generator"
   s.description = "Tired of writing CSS sprites by hand? well this simple gem will make it easy"
   s.authors = ["Juan GiménezSilva"]
